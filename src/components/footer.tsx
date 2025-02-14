b3e2b26df8940ab194851ee2a2121418a84710fa
Aremu <aremu.script@gmail.com>
2025-02-14 19:40:12 +0100

    updatig

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Mail, Phone, MapPin, ArrowUp } from 'lucide-react';
+import { Mail, Phone, MapPin,} from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
